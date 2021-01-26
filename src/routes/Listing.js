2e87e4bac3d7e4a3faa0f0647a99df3b3889af0c
shivani-priya <shivani.priya762@gmail.com>
2021-01-26 22:52:28 +0530

    1 file modified

--- src/routes/Listing.js
+++ src/routes/Listing.js
@@ -3,13 +3,19 @@ import { Link } from "react-router-dom";
 import Card from "@material-ui/core/Card";
 import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
+
 export default function Listing() {
   const [error, setError] = useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
+  const [offset, setOffset] = useState(0);
   const [pokemons, setPokemons] = useState([]);
 
-  useEffect(() => {
-    fetch("https://pokeapi.co/api/v2/pokemon/")
+  function fetchPokemons(offset, limit = 20) {
+    setIsLoaded(false);
+    setPokemons([]);
+    fetch(
+      "https://pokeapi.co/api/v2/pokemon/?limit=" + limit + "&offset=" + offset
+    )
       .then(res => res.json())
       .then(
         result => {
@@ -21,6 +27,15 @@ export default function Listing() {
           setError(error);
         }
       );
+  }
+
+  function loadMore() {
+    setOffset(offset + 20);
+    fetchPokemons(offset, 20);
+  }
+
+  useEffect(() => {
+    fetchPokemons(offset, 20);
   }, []);
 
   if (error) {
@@ -29,27 +44,42 @@ export default function Listing() {
     return <div>Loading...</div>;
   } else {
     return (
-      <div className="listing-container">
-        {pokemons.map((pokemon, index) => (
-          <Card className="listing-card" key={index + 1}>
-            <CardContent>
-              <Link to={"/details/" + (index + 1)}>
-                Id: {index + 1}
-                <br />
-                Name: {pokemon.name}
-                <br />
-                <img
-                  src={
-                    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/" +
-                    (index + 1) +
-                    ".png"
+      <>
+        <div className="listing-container">
+          {pokemons.map((pokemon, index) => (
+            <Card
+              className="listing-card"
+              key={pokemon.url.slice(pokemon.url.indexOf("/pokemon/") + 9, -1)}
+            >
+              <CardContent>
+                <Link
+                  to={
+                    "/details/" +
+                    pokemon.url.slice(pokemon.url.indexOf("/pokemon/") + 9, -1)
                   }
-                />
-              </Link>
-            </CardContent>
-          </Card>
-        ))}
-      </div>
+                >
+                  Id:{" "}
+                  {pokemon.url.slice(pokemon.url.indexOf("/pokemon/") + 9, -1)}
+                  <br />
+                  Name: {pokemon.name}
+                  <br />
+                  <img
+                    src={
+                      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/" +
+                      pokemon.url.slice(
+                        pokemon.url.indexOf("/pokemon/") + 9,
+                        -1
+                      ) +
+                      ".png"
+                    }
+                  />
+                </Link>
+              </CardContent>
+            </Card>
+          ))}
+        </div>
+        <button onClick={() => loadMore()}>Load More</button>
+      </>
     );
   }
 }
