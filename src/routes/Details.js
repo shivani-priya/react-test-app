4b686d2a7d7ddd134663e36e4b80d8ad6b06b5e4
shivani-priya <shivani.priya762@gmail.com>
2021-01-30 02:22:35 +0530

    1 file modified

--- src/routes/Details.js
+++ src/routes/Details.js
@@ -1,13 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
 export default function Details() {
   const { pokeId } = useParams();
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [details, setDetails] = useState([]);
+  function fetchDetails() {
+    fetch("https://pokeapi.co/api/v2/pokemon/1")
+      .then(res => res.json())
+      .then(result => {
+        setIsLoaded(true);
+        setDetails(result.forms);
+        // console.log(details);
+      });
+  }
+  useEffect(() => {
+    fetchDetails();
+  }, []);
   return (
     <>
       <h1>Details</h1>
 
       <div>Loading details for {pokeId}</div>
+      <div>
+        {details.map((ability, index) => (
+          <p key={index}> Ability Name: {abilities.ability.name}</p>
+        ))}
+      </div>
     </>
   );
 }
